cae6362b8dd0e157b065112e8dda98fa442b2222
bungin-2310 <136599375+bungin-2310@users.noreply.github.com>
2026-04-22 22:24:47 +0900

    ふぃ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
             <head>
                 <meta charSet="utf-8" />
                 <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700&family=Zen+Old+Mincho:wght@400&display=swap" rel="stylesheet" />
             </head>
 
             <body>
